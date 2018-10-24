c874453785611aea3b70640929aec70b35cd05c4
Ulaş Güney Genç <ulasguneygenc@gmail.com>
2018-10-24 20:21:05 +0300

    A basic level of color switch game
    You can try it on Unity Engine.

---
+++ Color Switch/Assets/Player.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+
+    public float jumpForce = 10f;
+    public float wayForce = 5f;
+
+    public SpriteRenderer sr;
+    public Color colorCyan;
+    public Color colorYellow;
+    public Color colorMagenta;
+    public Color colorPink;
+
+
+    public string currentColor;
+
+    public Rigidbody2D rb;
+
+
+    private void Start()
+    {
+        SetRandomColor(); 
+    }
+    // Update is called once per frame
+    void Update () {
+		if(Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+       
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "ColorChanger")
+        {
+            SetRandomColor();
+            Destroy(col.gameObject);
+            return;
+        }
+        if(col.tag != currentColor)
+        {
+            Debug.Log("GAME OVER!");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+       
+    }
+    void SetRandomColor()
+    {
+        int index = Random.Range(0, 4);
+        switch (index)
+        {
+            case 0:
+                currentColor = "Cyan";
+                sr.color = colorCyan;
+                break;
+            case 1:
+                currentColor = "Yellow";
+                sr.color = colorYellow;
+                break;
+            case 2:
+                currentColor = "Magenta";
+                sr.color = colorMagenta;
+                break;
+            case 3:
+                currentColor = "Pink";
+                sr.color = colorPink;
+                break;
+                
+            
+        }
+    }
+
+
+}
